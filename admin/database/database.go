e43cc7f1115fc205d3ef16c9145f704723f5f379
ogznglr <oguzzhan_4545@hotmail.com>
2022-09-03 22:04:10 +0300

    new commit

--- admin/database/database.go
+++ admin/database/database.go
@@ -9,18 +9,14 @@ var DB *gorm.DB
 
 func Connection() {
 	str := "root:password@tcp(db:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
-	if err != nil {
-		panic("Couldn't connect with database")
-	}
+	db, _ := gorm.Open(mysql.Open(str), &gorm.Config{})
 
 	DB = db
 }
 
 func Migrate(d interface{}) error {
-	err := DB.AutoMigrate(d)
-	if err != nil {
-		panic("Database güncellenemedi")
-	}
+
+	DB.AutoMigrate(d)
+
 	return nil
 }
--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM golang:alpine3.12
+FROM golang:alpine3.16
 WORKDIR /go/src/goweb
 COPY . .
-CMD ["/go/src/goweb/blog"]
+CMD ["/go/src/goweb/blog","+rwx"]
