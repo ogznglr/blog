e935bd994bbef82125157e89db80aabdfeda73d8
ogznglr <oguzzhan_4545@hotmail.com>
2022-09-12 23:36:45 +0300

    tls deneme2

--- go.mod
+++ go.mod
@@ -3,17 +3,17 @@ module blog
 go 1.18
 
 require (
-	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
 	github.com/gofiber/fiber/v2 v2.37.1
-	github.com/gofiber/template v1.7.0
 	github.com/gosimple/slug v1.12.0
 	github.com/ogznglr/session v0.0.0-20220905063122-e33fbcf5ff12
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -22,8 +22,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -5,15 +5,12 @@ import (
 	"blog/admin/models"
 	"blog/routes"
 	"crypto/tls"
-	"fmt"
-	"time"
+	"log"
 
 	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
-	"github.com/gofiber/template/html"
 )
 
 func init() {
@@ -25,51 +22,95 @@ func init() {
 }
 
 func main() {
-	//Certificate
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("oguzhanguler.dev"),
-		Cache:      autocert.DirCache("certs"),
-	}
 
-	TLSConfig := &tls.Config{
-		GetCertificate: certManager.GetCertificate,
-	}
+	// Fiber instance
+	app := fiber.New()
 
-	// listener, _ := net.Listen("tcp", ":8080")
-	listener, _ := tls.Listen("tcp", ":443", TLSConfig)
-	//-------------------------------------------------------------------
+	// Routes
+	routes.Setup(app)
 
-	engine := html.New("./views", ".html")
-	engine.AddFuncMap(fiber.Map{
-		"getCategoryName": func(categoryId int) string {
-			return models.Category{}.Get(categoryId).Name
-		},
-		"getRange": func(postNumber int) int {
-			if postNumber <= 3 {
-				return 1
-			}
-			return postNumber / 3
-		},
-		"getDate": func(t time.Time) string {
-			return fmt.Sprintf("%d %s %d", t.Day(), t.Month().String(), t.Year())
-		},
-	})
-
-	engine.Reload(true) //updates files in each rendering
-
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 	}))
 
-	app.Use(encryptcookie.New(encryptcookie.Config{
-		Key: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-	}))
+	// Let’s Encrypt has rate limits: https://letsencrypt.org/docs/rate-limits/
+	// It's recommended to use it's staging environment to test the code:
+	// https://letsencrypt.org/docs/staging-environment/
 
-	routes.Setup(app)
-	app.Listener(listener)
+	// Certificate manager
+	m := &autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		// Replace with your domain
+		HostPolicy: autocert.HostWhitelist("oguzhanguler.dev"),
+		// Folder to store the certificates
+		Cache: autocert.DirCache("./certs"),
+	}
 
+	// TLS Config
+	cfg := &tls.Config{
+		// Get Certificate from Let's Encrypt
+		GetCertificate: m.GetCertificate,
+		// By default NextProtos contains the "h2"
+		// This has to be removed since Fasthttp does not support HTTP/2
+		// Or it will cause a flood of PRI method logs
+		// http://webconcepts.info/concepts/http-method/PRI
+		NextProtos: []string{
+			"http/1.1", "acme-tls/1",
+		},
+	}
+	ln, err := tls.Listen("tcp", ":443", cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start server
+	log.Fatal(app.Listener(ln))
 }
+
+// //Certificate
+// certManager := autocert.Manager{
+// 	Prompt:     autocert.AcceptTOS,
+// 	HostPolicy: autocert.HostWhitelist("oguzhanguler.dev"),
+// 	Cache:      autocert.DirCache("certs"),
+// }
+
+// TLSConfig := &tls.Config{
+// 	GetCertificate: certManager.GetCertificate,
+// }
+// TLSConfig.Certificates = append(TLSConfig.Certificates, certManager.TLSConfig().Certificates...)
+
+// // listener, _ := net.Listen("tcp", ":8080")
+// listener, _ := tls.Listen("tcp", ":443", TLSConfig)
+// //-------------------------------------------------------------------
+
+// engine := html.New("./views", ".html")
+// engine.AddFuncMap(fiber.Map{
+// 	"getCategoryName": func(categoryId int) string {
+// 		return models.Category{}.Get(categoryId).Name
+// 	},
+// 	"getRange": func(postNumber int) int {
+// 		if postNumber <= 3 {
+// 			return 1
+// 		}
+// 		return postNumber / 3
+// 	},
+// 	"getDate": func(t time.Time) string {
+// 		return fmt.Sprintf("%d %s %d", t.Day(), t.Month().String(), t.Year())
+// 	},
+// })
+
+// engine.Reload(true) //updates files in each rendering
+
+// app := fiber.New(fiber.Config{
+// 	Views: engine,
+// })
+// app.Use(cors.New(cors.Config{
+// 	AllowCredentials: true,
+// }))
+
+// app.Use(encryptcookie.New(encryptcookie.Config{
+// 	Key: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+// }))
+
+// routes.Setup(app)
+// app.Listener(listener)
